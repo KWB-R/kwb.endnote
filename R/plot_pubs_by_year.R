37e1de6e11a8803383fa30962217ecd20dda524e
mrustl <michael.rustler@kompetenz-wasser.de>
2019-01-11 15:11:17 +0100

    Fix plot

--- R/plot_pubs_by_year.R
+++ R/plot_pubs_by_year.R
@@ -5,6 +5,7 @@
 #' @return plot with publications by year
 #' @export
 #' @import ggplot2
+#' @importFrom stringr str_trim
 #' @examples
 #' endnote_list <- kwb.endnote::create_endnote_list()
 #' refs_df <- kwb.endnote::create_references_df(endnote_list)
@@ -12,15 +13,18 @@
 #'
 plot_pubs_by_year <- function(refs_df) {
   refs_df %>%
+  dplyr::mutate(dates_year = stringr::str_trim(.data$dates_year)) %>%
   dplyr::count(.data$ref_type_name, .data$dates_year) %>%
   ggplot2::ggplot(ggplot2::aes(x = .data$dates_year,
+                               y = .data$n,
                                fill = .data$ref_type_name)) +
-  ggplot2::geom_bar() +
+  ggplot2::geom_col() +
   ggplot2::theme_minimal() +
   ggplot2::labs(x = "Year",
-                y = "Number Of Publications") +
+                y = "Number Of Publications",
+                fill = "Publication Type",
+                title = "Publications By Year") +
   ggplot2::coord_flip() +
-  ggplot2::theme(legend.position = "right",
-                 legend.title = ggplot2::element_blank())
+  ggplot2::theme(legend.position = "right")
 
 }
