e36a8404b40930948e6ae0516c87797646eccc7f
hsonne <hauke.sonnenberg@gmx.de>
2019-01-12 11:55:06 +0100

    Use .data within dplyr::mutate() fixing #4
    - use names() instead of setNames() to avoid
      duplication of "refs_df_list"
    - improve indentation

--- R/write_references_df_to_xlsx.R
+++ R/write_references_df_to_xlsx.R
@@ -13,18 +13,20 @@
 #' refs_df <- create_references_df(endnote_list)
 #' write_references_df_to_xlsx(refs_df)
 #' }
-write_references_df_to_xlsx <- function(refs_df,
-                                        file = "references.xlsx",
-                                        ...) {
+write_references_df_to_xlsx <- function(
+  refs_df, file = "references.xlsx", ...
+) {
+
   pub_types <- unique(refs_df$ref_type_name)
+
   refs_df_list <- lapply(pub_types, function(x) {
     refs_df[refs_df$ref_type_name == x, ] %>%
-      dplyr::select_if(~sum(!is.na(.)) > 0) %>%
-      dplyr::mutate(rec_number = as.numeric(rec_number)) %>%
+      dplyr::select_if(~sum(! is.na(.)) > 0) %>%
+      dplyr::mutate(rec_number = as.numeric(.data$rec_number)) %>%
       dplyr::arrange(dplyr::desc(.data$rec_number))
   })
 
-  refs_df_list <- stats::setNames(refs_df_list, pub_types)
+  names(refs_df_list) <- pub_types
 
   openxlsx::write.xlsx(refs_df_list, file, ...)
 }
