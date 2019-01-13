4f5f387efea28ec75ed1010a7a2c9b3db9addb79
hsonne <hauke.sonnenberg@gmx.de>
2019-01-13 13:39:00 +0100

    Return early in get_abstract()
    - now, NA is returned in case of NULL, before, the
      string "NA" war returned as paste converts NA to "NA"
    - do not use intermediate variable "abstract_vector"
      set arg "collapse" first to improve indentation

--- R/create_references_df_from_list.R
+++ R/create_references_df_from_list.R
@@ -1,6 +1,7 @@
 #' @noRd
 #' @keywords internal
 null_to_na <- function(x, na_fill = NA_character_) {
+
   if (is.null(x[[1]])) na_fill else x[[1]]
 }
 
@@ -11,17 +12,18 @@ null_to_na <- function(x, na_fill = NA_character_) {
 #' @export
 #' @importFrom dplyr bind_cols
 get_abstract <- function(record_list) {
+
   abstract <- record_list$record$abstract
 
   if (is.null(abstract)) {
-    abstract_vector <- NA_character_
-  } else {
-    abstract_vector <- lapply(seq_along(abstract), function(i) {
-      null_to_na(abstract[[i]][[1]])
-    })
+
+    return(NA_character_)
   }
 
-  paste(abstract_vector, collapse = "")
+  paste(collapse = "", lapply(seq_along(abstract), function(i) {
+
+    null_to_na(abstract[[i]][[1]])
+  }))
 }
 
 #' Helper function: get authors from list for a reference
