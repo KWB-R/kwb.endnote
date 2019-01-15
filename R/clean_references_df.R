2ec6e18f61cc9a6ed7df37d39a9b2ecc70ac8a61
hsonne <hauke.sonnenberg@gmx.de>
2019-01-15 23:58:24 +0100

    Clean clean_references_df.R
    - improve indentation

--- R/clean_references_df.R
+++ R/clean_references_df.R
@@ -8,20 +8,16 @@
 #' @importFrom kwb.utils catAndRun
 clean_dois <- function(dois, dbg = TRUE) {
 
-  kwb.utils::catAndRun("Clean 'DOI'", expr = {
+  remove <- stringr::str_remove_all
 
-  dois %>%
-    stringr::str_remove_all("^http(s)?://") %>%
-    stringr::str_remove_all("^(dx\\.)?doi\\.org/") %>%
-    stringr::str_remove_all(stringr::regex("^doi:?\\s?",
-                                           ignore_case = TRUE)) %>%
-    stringr::str_trim()
-
-},
-dbg = dbg)
-
+  kwb.utils::catAndRun("Clean 'DOI'", dbg = dbg, expr = {
+    dois %>%
+      remove("^http(s)?://") %>%
+      remove("^(dx\\.)?doi\\.org/") %>%
+      remove(stringr::regex("^doi:?\\s?", ignore_case = TRUE)) %>%
+      stringr::str_trim()
+  })
 }
-
 
 #' Helper function: clean project names
 #'
@@ -34,36 +30,37 @@ dbg = dbg)
 #' @export
 #' @importFrom stringr str_remove_all str_replace_all regex str_trim
 #' @importFrom kwb.utils catAndRun
-clean_project_names <- function(project_names, replace_na = FALSE,
-                                replace_na_text = "add_project_name",
-                                dbg = TRUE) {
+clean_project_names <- function(
+  project_names, replace_na = FALSE, replace_na_text = "add_project_name",
+  dbg = TRUE
+) {
 
-kwb.utils::catAndRun("Clean 'Project Names'", expr = {
+  kwb.utils::catAndRun("Clean 'Project Names'", dbg = dbg, expr = {
 
-  project_names <- project_names %>%
-    stringr::str_replace_all("\\s+?/", ",") %>%
-    stringr::str_remove_all("-") %>%
-    stringr::str_remove_all("_.*") %>%
-    stringr::str_replace_all(stringr::regex("^techneau.*",
-                                            ignore_case = TRUE),
-                             "TECHNEAU") %>%
-    stringr::str_remove_all("\\s+?") %>%
-    stringr::str_trim() %>%
-    tolower()
+    project_names <- project_names %>%
+      stringr::str_replace_all("\\s+?/", ",") %>%
+      stringr::str_remove_all("-") %>%
+      stringr::str_remove_all("_.*") %>%
+      stringr::str_replace_all(
+        stringr::regex("^techneau.*", ignore_case = TRUE), "TECHNEAU"
+      ) %>%
+      stringr::str_remove_all("\\s+?") %>%
+      stringr::str_trim() %>%
+      tolower()
 
-  if (replace_na) project_names[is.na(project_names)] <- replace_na_text
+    if (replace_na) {
+      project_names[is.na(project_names)] <- replace_na_text
+    }
 
-  project_names
-},
- dbg = dbg)
-
+    project_names
+  })
 }
 
 
-if(FALSE) {
-  stringr::str_split(clean_project_names(project_names),
-                     ",",
-                     simplify = TRUE) %>%
+if (FALSE) {
+
+  clean_project_names(project_names) %>%
+    stringr::str_split(",", simplify = TRUE) %>%
     tibble::as.tibble() %>%
     dplyr::mutate_if(is.character, list(~dplyr::na_if(., NA_character_)))
 }
@@ -80,36 +77,40 @@ if(FALSE) {
 #' @export
 #' @importFrom stringr str_detect
 #' @importFrom kwb.utils catAndRun
-clean_author_names <- function(author_names, replace_na = FALSE,
-                                replace_na_text = "add_author_lastname_semicolon_firstname",
-                               dbg = TRUE) {
+clean_author_names <- function(
+  author_names, replace_na = FALSE,
+  replace_na_text = "add_author_lastname_semicolon_firstname", dbg = TRUE
+) {
 
-kwb.utils::catAndRun("Clean and Check 'Author Names'", expr = {
+  kwb.utils::catAndRun("Clean and Check 'Author Names'", dbg = dbg, expr = {
 
-  multiple_authors_idx <- which(unlist(lapply(author_names, function(x)
-    length(stringr::str_split(x, pattern = ",")[[1]]))) > 2)
+    multiple_authors_idx <- which(
+      unlist(lapply(author_names, function(x)
+        length(stringr::str_split(x, pattern = ",")[[1]])
+      )) > 2
+    )
 
-  author_names[multiple_authors_idx] <- "fix_multiple_authors_per_line"
+    author_names[multiple_authors_idx] <- "fix_multiple_authors_per_line"
 
+    firstname_lastname_idx <- which(
+      stringr::str_detect(author_names, "^\\s?\\w+\\.?\\s+\\w+") &
+        ! stringr::str_detect(author_names, ",")
+    )
 
-  firstname_lastname_idx <- which(stringr::str_detect(author_names,
-                                                      "^\\s?\\w+\\.?\\s+\\w+") &
-   !stringr::str_detect(author_names, ","))
+    author_names[firstname_lastname_idx] <- c(
+      "fix_firstname_lastname_with_lastname_semicolon_firstname"
+    )
 
+    # author_names <- author_names %>%
+    #   stringr::str_trim()
 
-  author_names[firstname_lastname_idx] <- "fix_firstname_lastname_with_lastname_semicolon_firstname"
+    if (replace_na) {
 
+      author_names[is.na(author_names)] <- replace_na_text
+    }
 
-  # author_names <- author_names %>%
-  #   stringr::str_trim()
-
-  if (replace_na) author_names[is.na(author_names)] <- replace_na_text
-
-  author_names
-},
-dbg = dbg)
-
-
+    author_names
+  })
 }
 
 #' Helper function: clean access information
@@ -122,35 +123,36 @@ dbg = dbg)
 #' @return vector with cleaned accessibility information
 #' @export
 #' @importFrom stringr str_remove_all str_replace_all regex str_trim
-clean_accessibility <- function(access,  replace_na = FALSE,
-                                replace_na_text = "add_public_or_confidential",
-                                dbg = TRUE) {
+clean_accessibility <- function(
+  access, replace_na = FALSE, replace_na_text = "add_public_or_confidential",
+  dbg = TRUE
+) {
 
-kwb.utils::catAndRun("Clean 'Accessibility'", expr = {
+  replace_all <- function(string, pattern, replacement) {
+    stringr::str_replace_all(
+      string = string,
+      pattern = stringr::regex(pattern, ignore_case = TRUE),
+      replacement = replacement
+    )
+  }
 
-  access <- access %>%
-    stringr::str_replace_all(stringr::regex("PU.*",ignore_case = TRUE),
-                             "public") %>%
-    stringr::str_replace_all(stringr::regex("public.*",ignore_case = TRUE),
-                             "public") %>%
-    stringr::str_replace_all(stringr::regex("privat.*",ignore_case = TRUE),
-                             "confidential") %>%
-    stringr::str_replace_all(stringr::regex("intern.*",ignore_case = TRUE),
-                             "confidential") %>%
-    stringr::str_replace_all(stringr::regex("confident.*",ignore_case = TRUE),
-                             "confidential") %>%
-    stringr::str_trim()
+  kwb.utils::catAndRun("Clean 'Accessibility'", dbg = dbg, expr = {
 
-  if (replace_na) access[is.na(access)] <- replace_na_text
+    access <- access %>%
+      replace_all("PU.*", "public") %>%
+      replace_all("public.*", "public") %>%
+      replace_all("privat.*", "confidential") %>%
+      replace_all("intern.*", "confidential") %>%
+      replace_all("confident.*", "confidential") %>%
+      stringr::str_trim()
 
-  access
+    if (replace_na) {
+      access[is.na(access)] <- replace_na_text
+    }
 
-},
-dbg = dbg)
-
+    access
+  })
 }
-
-
 
 #' Clean References Dataframe
 #'
@@ -166,36 +168,31 @@ dbg = dbg)
 #' refs_clean_df <- clean_references_df(endnote_list)
 #' head(refs_clean_df)
 #' }
-clean_references_df <- function(endnote_list,
-                                replace_na = FALSE,
-                                dbg = TRUE) {
-
+clean_references_df <- function(endnote_list, replace_na = FALSE, dbg = TRUE) {
 
   refs_df <- create_references_df(endnote_list,collapse = TRUE)
 
-  refs_df <- kwb.utils::catAndRun("\nClean and Check 'Author Names'",
-  expr = {
-  col_authors <- names(refs_df)[stringr::str_detect(names(refs_df), "author")]
-  for(col_author in col_authors) {
-    replace_author_na <- if(col_author == "author01") { TRUE } else { FALSE }
-    refs_df[[col_author]]  <- clean_author_names(refs_df[[col_author]],
-        replace_na = replace_author_na,
-        dbg = FALSE)
-  }
-  refs_df
-  },
-  dbg = dbg)
+  refs_df <- kwb.utils::catAndRun(
 
+    messageText = "\nClean and Check 'Author Names'", dbg = dbg, expr = {
 
-  refs_df %>%
-  dplyr::mutate(electronic_resource_num = clean_dois(.data$electronic_resource_num,
-                                                     dbg = dbg),
-                custom2 = clean_project_names(.data$custom2,
-                                              replace_na = replace_na,
-                                              dbg = dbg),
-                custom3 = clean_accessibility(.data$custom3,
-                                              replace_na = replace_na,
-                                              dbg = dbg))
+      is_author <- stringr::str_detect(names(refs_df), "author")
 
+      col_authors <- names(refs_df)[is_author]
+
+      for (col_author in col_authors) {
+        replace_author_na <- (col_author == "author01")
+        refs_df[[col_author]]  <- clean_author_names(
+          refs_df[[col_author]], replace_na = replace_author_na, dbg = FALSE
+        )
+      }
+
+      refs_df
+    }
+  )
+
+  refs_df %>% dplyr::mutate(
+    electronic_resource_num = clean_dois(.data$electronic_resource_num, dbg = dbg),
+    custom2 = clean_project_names(.data$custom2, replace_na, dbg = dbg),
+    custom3 = clean_accessibility(.data$custom3, replace_na, dbg = dbg))
 }
-
